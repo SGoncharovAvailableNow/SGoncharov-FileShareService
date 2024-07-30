ebf73ce31fea72b2c1b8d7379cd2145948c0b5aa
SGoncharovAvailableNow <s.goncharov@sh.dunice.net>
2024-07-30 10:53:33 +0300

    Delete timer from AutoDeletingService, add task.delay to executeAsync

--- SGoncharovFileSharingService/AutoDeletingServices/AutoDeletingService.cs
+++ SGoncharovFileSharingService/AutoDeletingServices/AutoDeletingService.cs
@@ -7,8 +7,7 @@ namespace SGoncharovFileSharingService;
 public class AutoDeletingService : BackgroundService, IDisposable
 {
     private readonly IWebHostEnvironment _webHostEnvironment;
-    private readonly IServiceScopeFactory  _serviceScopeFactory;
-    private Timer? _timer = null;
+    private readonly IServiceScopeFactory _serviceScopeFactory;
 
     public AutoDeletingService(IWebHostEnvironment webHostEnvironment, IServiceScopeFactory scopeFactory)
     {
@@ -18,7 +17,14 @@ public class AutoDeletingService : BackgroundService, IDisposable
 
     protected override Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        _timer = new Timer(DeleteSharingFiles, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+        Task.Run(async () =>
+        {
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                await Task.Delay(TimeSpan.FromDays(1), stoppingToken);
+                DeleteSharingFiles(stoppingToken);
+            }
+        });
         return Task.CompletedTask;
     }
 
@@ -30,7 +36,7 @@ public class AutoDeletingService : BackgroundService, IDisposable
             var sharedFilesDirectory = Directory.EnumerateFiles(_webHostEnvironment.WebRootPath);
             foreach (var sharedFile in sharedFilesDirectory)
             {
-                if ((File.GetCreationTimeUtc(sharedFile).AddDays(1)) < DateTime.UtcNow)
+                if ((File.GetCreationTimeUtc(sharedFile).AddSeconds(10)) < DateTime.UtcNow)
                 {
                     try
                     {
@@ -44,10 +50,5 @@ public class AutoDeletingService : BackgroundService, IDisposable
                 }
             }
         }
-    }
-
-    public override void Dispose()
-    {
-        _timer?.Dispose();
     }
 }
